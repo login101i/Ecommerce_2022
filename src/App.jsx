43413ba02ab30ddf166c64ec42eef1cc526ad9d7
Maciej <maciejkruszyniak@gmail.com>
2022-05-11 23:43:09 +0200

    creating and styling Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
-const App = () => {
-  return <div>Hello world!</div>;
-};
+import React, {Component} from 'react'
+import { HomePage } from "./homepage.component";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HomePage />
+      </div>
+    );
+  }
+}
 
 export default App;

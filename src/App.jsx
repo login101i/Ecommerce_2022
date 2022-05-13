a6de1b58c42129fa4445398a6faebf7b651e0e86
Maciej <maciejkruszyniak@gmail.com>
2022-05-13 21:02:09 +0200

    React Router Dom, useNaviagate instead WithRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import { HomePage } from "./pages/homepage.component";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import "./App.css";
+
+const HatsPage = () => {
+  return <div>Hello from HatsPage</div>;
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HomePage />
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route path='/hats' component={<HatsPage/>} />
+          </Routes>
+        </BrowserRouter>
       </div>
     );
   }
--- src/components/menu_item/menu-item.component.jsx
+++ src/components/menu_item/menu-item.component.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-import './menu-item.styles.scss';
+import "./menu-item.styles.scss";
 
-export const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
+export const MenuItem = ({
+  title,
+  imageUrl,
+  size,
+  history,
+  linkUrl,
+  match
+}) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={`${size} menu-item`} onClick={() => navigate(linkUrl)}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};

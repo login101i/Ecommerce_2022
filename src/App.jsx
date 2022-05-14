22c7bff8b456b218a2ac5e8e0fb362fb0724393b
Maciej <maciejkruszyniak@gmail.com>
2022-05-14 16:37:51 +0200

    sign-in, form and button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { HomePage } from "./pages/homepage.component";
+import { HomePage } from "./pages/home/homepage.component";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import { ShopPage } from "./pages/shop/shop.component.jsx";
 import { Header } from "./components/header/header.component";
-
+import { SignInAndSignUpPage } from "./pages/sign-in-and-sign-upp/sign-in-sign-upp.component";
 class App extends Component {
   render() {
     return (
@@ -17,6 +17,7 @@ class App extends Component {
           <Routes>
             <Route exact path="/" element={<HomePage />} />
             <Route exact path="/shop" element={<ShopPage />} />
+            <Route exact path="/sign-in" element={<SignInAndSignUpPage />} />
           </Routes>
         </BrowserRouter>
       </div>

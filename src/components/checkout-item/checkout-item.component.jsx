4962390cd3176761e63b6082aac7227cb518b25f
Maciej <maciejkruszyniak@gmail.com>
2022-05-15 23:33:46 +0200

    added addItem and removeItem funcionality in cartCheckout

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,30 +1,43 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
-import './checkout-item.styles.scss';
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem
+} from "../../redux/cart/cart.actions";
 
-export const CheckoutItem = ({ cartItem }) => {
+import "./checkout-item.styles.scss";
+
+export const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   return (
-    <div className='checkout-item'>
-      <div className='image-container'>
-        <img src={imageUrl} alt='item' />
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
       </div>
-      <span className='name'>{name}</span>
-      <span className='quantity'>
-        <div className='arrow' >
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
           &#10094;
         </div>
-        <span className='value'>{quantity}</span>
-        <div className='arrow'>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
           &#10095;
         </div>
       </span>
-      <span className='price'>{price}</span>
-      <div className='remove-button' >
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
         &#10005;
       </div>
     </div>
   );
 };
 
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item))
+});
 
+export const CheckoutItemHOC = connect(null, mapDispatchToProps)(CheckoutItem);
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import {CheckoutItem} from '../../components/checkout-item/checkout-item.component';
+import {CheckoutItemHOC} from '../../components/checkout-item/checkout-item.component';
 // import {StripeCheckoutButton} from '../../components/stripe-button/stripe-button.component';
 
 import {
@@ -32,7 +32,7 @@ export const CheckoutPage = ({ cartItems, total }) => (
       </div>
     </div>
     {cartItems.map(cartItem => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      <CheckoutItemHOC key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className='total'>TOTAL: ${total}</div>
     <div className='test-warning'>

4a9644822bb3a91a39928d4aef3d5a25543dae9d
login101i <maciejkruszyniak@gmail.com>
2022-06-01 20:53:29 +0200

    Profiler for performance issues

--- client/src/pages/home/homepage.component.jsx
+++ client/src/pages/home/homepage.component.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { Profiler } from "react";
 
 import { DirectoryHOC } from "../../components/directory/directory.component";
 
 import { HomePageContainer } from "./homepage.styles";
 
 const HomePage = () => {
- 
   return (
     <HomePageContainer>
-      <DirectoryHOC />
+      <Profiler
+        id="directory"
+        onRender={(id, phase, actualDuration) => {
+          console.log({
+            id,
+            phase,
+            actualDuration
+          });
+        }}
+      >
+        <DirectoryHOC />
+      </Profiler>
     </HomePageContainer>
   );
 };
